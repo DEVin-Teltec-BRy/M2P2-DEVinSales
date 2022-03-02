92fd217006d4ccb32f6b03a491af72f19ced4213
Victor Bertram <BERTRAMVICTOR61@GMAIL.COM>
2022-03-02 18:55:21 -0300

    Seeders Users Adicionadas

---
+++ src/utils/constants/users.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt')
+const saltRounds = 5
+const salt = bcrypt.genSaltSync(saltRounds)
+
+const createUser = (id,name,email,birth_date,password)=>{
+    const passwordHash = bcrypt.hashSync(password,salt)
+    return {
+        id,
+        name,
+        email,
+        birth_date,
+        password: passwordHash,
+        created_at: new Date(),
+        updated_at: new Date(),
+
+    }
+}
+
+module.exports = {
+    User1: createUser(1,'Sabrina','sabrina1@gmail.com','12/01/1980','K79OUIOj'),
+    User2:createUser(2,'Daniel','Daniel12@gmail.com','02/02/1985','123456789'),
+    User3:createUser(3,'Luciano','luciano3@gmail.com','05/04/2000','Lu123.1'),
+    User4:createUser(4,'Victor','Bertramvictor123@gmail.com','22/08/2000','VictorD12.'),
+
+}
