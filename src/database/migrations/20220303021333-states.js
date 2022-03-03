d3747ba07078c90f2eb96cf44ee1411547accdbd
raulvictorvieira <raulvictorsv@gmail.com>
2022-03-02 23:50:02 -0300

    configurações iniciais da migration states

--- src/database/migrations/20220303021333-states.js
+++ src/database/migrations/20220303021333-states.js
@@ -1,21 +1,39 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'states',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        initials: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      }
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('states');
   }
 };
