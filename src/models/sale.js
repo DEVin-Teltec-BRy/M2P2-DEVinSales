c59d1b7a8cd05a1a0f96da05d8032cd02430e991
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2022-03-02 21:21:59 -0300

    Model Sale

--- src/models/sale.js
+++ src/models/sale.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Sale.hasMany(User,{
+        foreignKey:'id',
+        constraints:true
+      })
     }
   }
   Sale.init({
