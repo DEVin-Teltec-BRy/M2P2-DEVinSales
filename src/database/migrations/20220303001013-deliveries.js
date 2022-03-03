6abb6e647d3f4b84df538137718b2b984643869c
Bruno Pacheco <bruno.p94@gmail.com>
2022-03-03 06:30:56 -0300

    definições da tabela do migrations: deliveries.js

--- src/database/migrations/20220303001013-deliveries.js
+++ src/database/migrations/20220303001013-deliveries.js
@@ -2,20 +2,46 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'deliveries',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },        
+        address_id: {
+          type: Sequelize.INTEGER,
+          references: {model: 'addresses', key: 'id'},
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          allowNull: false,
+        },
+        sale_id: {
+          type: Sequelize.INTEGER,
+          references: {model: 'sales', key: 'id'},
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          allowNull: false,
+        },
+        delivery_forecast: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      }
+    );
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('deliveries');
   }
 };
