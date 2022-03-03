d51f3404a5024e4fd4a3d771205e778028288559
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2022-03-03 11:50:31 -0300

    Padronizando Model Sale

--- src/models/sale.js
+++ src/models/sale.js
@@ -1,29 +1,38 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+
+const { Model, DataTypes} = require('sequelize');
+
   class Sale extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+ static init(sequelize) {
+   super.init(
+     {
+       seller_id:{
+       type:DataTypes.INTEGER,
+         allowNull:false,
+     },
+     buyer_id:{
+      type:DataTypes.INTEGER,
+        allowNull:false,
+    },
+    dt_sale:{
+      type:DataTypes.DATE,
+      allowNull:false
+    }
+
+     },
+    { sequelize,
+     modelName:"Sale"
+    }
+   )
+ }
     static associate(models) {
-      Sale.hasMany(User,{
+      Sale.hasMany(models.User,{
         foreignKey:'id',
+        through:'user',
         constraints:true
       })
     }
   }
-  Sale.init({
-    buyer_id: DataTypes.INTEGER,
-    seller_id: DataTypes.INTEGER,
-    dt_sale: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'sale',
-  });
-  return Sale;
-};
 
+
+module.exports = Sale
