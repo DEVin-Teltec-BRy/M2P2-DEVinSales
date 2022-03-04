7297358bd5851372288cd20ff2fdc295ea9d2d8e
Lmsilvano <75850731+Lmsilvano@users.noreply.github.com>
2022-03-03 23:43:23 -0300

    Update ProductController.js

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -7,10 +7,11 @@ module.exports = {
         // #swagger.description = 'Endpoint para buscar produtos conforme critério query params'
 
         try {
-            const filter = req.query
-            console.log(filter)
-            console.log('aaaa')
-            return res.status(200).send({ message: filter });
+            const { name, price_min, price_max } = req.query
+            const priceMin = Number(price_min) ? price_min : 0
+            const priceMax = Number(price_max) ? price_max : Number.MAX_SAFE_INTEGER
+            console.log(priceMin, priceMax)
+            return res.status(200).send({ message: 'ok' });
         } catch (error) {
             const message = validateErrors(error);
             return res.status(400).send(message)
