f26dfd14410b7d273b432cc8689299573926e02a
sabrinamaral <sabrinamaral@hotmail.com>
2022-03-04 10:12:42 -0300

    ajustando formato da data na model user

--- src/models/User.js
+++ src/models/User.js
@@ -1,44 +1,50 @@
-const { DataTypes, Model } = require('sequelize')
-const { hashPassword } = require('../hooks/userHooks')
+const { DataTypes, Model } = require("sequelize");
+const { hashPassword } = require("../hooks/userHooks");
 
-class User extends Model{
-  static init(sequelize){
-    super.init({
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: {
-          msg: "E-mail deve ser único",
-      },
-      },
-      birth_date: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
-      },
-      password: {
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: {
           type: DataTypes.STRING,
-          allowNull: false
-        }
-    },{
-      sequelize,
-      hooks: {
-        beforeCreate: async (user)=>{
-          user.password = await hashPassword(user.password)
-        }
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: {
+            msg: "E-mail deve ser único",
+          },
+        },
+        birth_date: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+          get() {
+            const formatedDate = this.getDataValue("birth_date");
+            return formatedDate ? formatedDate.toLocaleDateString() : null;
+          },
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        hooks: {
+          beforeCreate: async (user) => {
+            user.password = await hashPassword(user.password);
+          },
+        },
       }
-    })
+    );
   }
-  static associate(models){
+  static associate(models) {
     this.belongsToMany(models.Role, {
-      foreignKey: 'user_id',
-      through: 'users_roles',
-      as: 'roles'
-    })
-
+      foreignKey: "user_id",
+      through: "users_roles",
+      as: "roles",
+    });
   }
 }
-module.exports = User
+module.exports = User;
