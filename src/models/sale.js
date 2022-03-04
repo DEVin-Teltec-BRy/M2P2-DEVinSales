df68a69b74b3f1d3bfdfd1ed0236f69a75ce984b
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2022-03-04 17:34:02 -0300

    Model e Migration

--- src/models/sale.js
+++ src/models/sale.js
@@ -1,38 +1,26 @@
 'use strict';
-
-const { Model, DataTypes} = require('sequelize');
-
+const User = require('./User')
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
   class Sale extends Model {
- static init(sequelize) {
-   super.init(
-     {
-       seller_id:{
-       type:DataTypes.INTEGER,
-         allowNull:false,
-     },
-     buyer_id:{
-      type:DataTypes.INTEGER,
-        allowNull:false,
-    },
-    dt_sale:{
-      type:DataTypes.DATE,
-      allowNull:false
-    }
-
-     },
-    { sequelize,
-     modelName:"Sale"
-    }
-   )
- }
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      Sale.hasMany(models.User,{
-        foreignKey:'id',
-        through:'user',
-        constraints:true
-      })
+      Sale.belongsToMany(models.User)
     }
   }
-
-
-module.exports = Sale
+  Sale.init({
+    seller_id: DataTypes.INTEGER,
+    buyer_id: DataTypes.INTEGER,
+    dt_sale: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Sale',
+  });
+  return Sale;
+};
