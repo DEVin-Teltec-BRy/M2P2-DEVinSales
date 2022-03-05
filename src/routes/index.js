9a819db28d8058e51c4fd253f5d9f634608a357e
leandro-da-silva <90876788+leandro-da-silva@users.noreply.github.com>
2022-03-05 17:26:37 -0300

    Criacao do endpoint5 time2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,20 @@
-const express = require('express')
-const routes = express.Router()
-const permissionRoutes = require('./v1/permissions.routes');
-const rolesRoutes = require('./v1/roles.routes');
-const usersRoutes = require('./v1/users.routes');
-<<<<<<< HEAD
-const salesRoutes = require('./v1/sales.routes');
+const express = require("express");
+const routes = express.Router();
+const permissionRoutes = require("./v1/permissions.routes");
+const rolesRoutes = require("./v1/roles.routes");
+const usersRoutes = require("./v1/users.routes");
+const salesRoutes = require("./v1/sales.routes");
+const productsRoutes = require("./v1/products.routes");
+routes.get("/", (req, res) => {
+  // #swagger.ignore = true
+  res.redirect("/api/v1/docs");
+});
+routes.use("/api/v1", [
+  usersRoutes,
+  rolesRoutes,
+  permissionRoutes,
+  salesRoutes,
+  productsRoutes,
+]);
 
-routes.get('/', (req, res)=>{
-=======
-const productsRoutes = require('./v1/products.routes');
-routes.get('/', (req, res) => {
->>>>>>> d0d59483945d75d7a872d072394060573692c240
-    // #swagger.ignore = true
-    res.redirect('/api/v1/docs')
-})
-routes.use('/api/v1', [
-    usersRoutes,
-    rolesRoutes,
-    permissionRoutes,
-<<<<<<< HEAD
-    salesRoutes
-=======
-    productsRoutes
->>>>>>> d0d59483945d75d7a872d072394060573692c240
-])
-
-module.exports = routes
+module.exports = routes;
--- src/routes/v1/sales.routes.js
+++ src/routes/v1/sales.routes.js
@@ -1,10 +1,11 @@
-const SaleController = require('../../controllers/SaleController');
-const express = require('express');
+const SaleController = require("../../controllers/SaleController");
+const express = require("express");
 
 const salesRoutes = express.Router();
 
+salesRoutes.get("/sales", SaleController.showSaler);
 
-salesRoutes.get('/sales', SaleController.showSaler);
-
-salesRoutes.post('/user/:user_id/sales', SaleController.create);
+salesRoutes.post("/user/:user_id/sales", SaleController.create);
+//Squad:TryCatch Tema:2 Endpoint:5
+salesRoutes.post("/user/:seller_id/item", SaleController.item);
 module.exports = salesRoutes;
