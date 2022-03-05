97592cd8c59581216d6d29a75c68b8693d8626c9
sabrinamaral <sabrinamaral@hotmail.com>
2022-03-04 22:28:15 -0300

    adição de constraint unique name na model product

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,19 +1,22 @@
-
-const { DataTypes, Model } = require('sequelize')
+const { DataTypes, Model } = require("sequelize");
 
 class Product extends Model {
-  static init(sequelize){
-    super.init({
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false
+  static init(sequelize) {
+    super.init(
+      {
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        suggested_price: {
+          type: DataTypes.DECIMAL,
+          allowNull: false,
+        },
       },
-      suggested_price: {
-        type: DataTypes.DECIMAL,
-        allowNull: false
-      }
-    }, {sequelize})
+      { sequelize }
+    );
   }
 }
 
-module.exports = Product
+module.exports = Product;
