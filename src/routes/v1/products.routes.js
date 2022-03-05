6569c936fa76be32c3ab54984b1c0fd40fd9d7ac
sabrinamaral <sabrinamaral@hotmail.com>
2022-03-04 22:22:13 -0300

    criação do endpoint post produtos

--- src/routes/v1/products.routes.js
+++ src/routes/v1/products.routes.js
@@ -1,9 +1,18 @@
-const ProductController = require('../../controllers/ProductController');
-const express = require('express')
+const ProductController = require("../../controllers/ProductController");
+const express = require("express");
 const productsRoutes = express.Router();
 const { onlyCanAccessWith } = require("../../middlewares/auth");
-const { READ } = require("../../utils/constants/permissions");
+const { READ, WRITE } = require("../../utils/constants/permissions");
 
-productsRoutes.get("/products", onlyCanAccessWith([READ]), ProductController.index);
+productsRoutes.get(
+  "/products",
+  onlyCanAccessWith([READ]),
+  ProductController.index
+);
+productsRoutes.post(
+  "/products",
+  onlyCanAccessWith([WRITE]),
+  ProductController.store
+);
 
 module.exports = productsRoutes;
