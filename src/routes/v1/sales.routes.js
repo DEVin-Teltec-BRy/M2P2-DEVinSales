9ca67c19da1e017476801bbda7a2493e83b1df2a
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2022-03-06 09:39:33 -0300

    rotas sales sem auth

--- src/routes/v1/sales.routes.js
+++ src/routes/v1/sales.routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const salesRoutes = express.Router();
 const { WRITE} = require('../../utils/constants/permissions')
 const {onlyCanAccessWith} = require('../../middlewares/auth')
-salesRoutes.post('/sales/:user_id/sale',onlyCanAccessWith([WRITE]), SaleController.createSale);
+salesRoutes.post('/sales/:user_id/sale',SaleController.createSale);
 salesRoutes.post('/sales/:user_id/buy', SaleController.createBuy);
 
 module.exports = salesRoutes;
