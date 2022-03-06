ad444603b072989806b71a4936a68f15d62dd8c4
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2022-03-05 21:20:13 -0300

    Auth

--- src/routes/v1/sales.routes.js
+++ src/routes/v1/sales.routes.js
@@ -1,8 +1,9 @@
 const SaleController = require('../../controllers/SaleController');
 const express = require('express')
 const salesRoutes = express.Router();
-
-salesRoutes.post('/sales/:user_id/sale',  SaleController.createSale);
+const { WRITE} = require('../../utils/constants/permissions')
+const {onlyCanAccessWith} = require('../../middlewares/auth')
+salesRoutes.post('/sales/:user_id/sale',onlyCanAccessWith([WRITE]), SaleController.createSale);
 salesRoutes.post('/sales/:user_id/buy', SaleController.createBuy);
 
 module.exports = salesRoutes;
--- src/utils/constants/users.js
+++ src/utils/constants/users.js
@@ -20,28 +20,27 @@ const createUser = (id, name, email, birth_date, password) => {
 
 module.exports = {
   User1: createUser(
-    1,
     "Sabrina",
     "sabrina1@gmail.com",
     "12/01/1980",
     "K79OUIOj"
   ),
   User2: createUser(
-    2,
+    
     "Daniel",
     "Daniel12@gmail.com",
     "02/02/1985",
     "123456789"
   ),
   User3: createUser(
-    3,
+    
     "Luciano",
     "luciano3@gmail.com",
     "05/04/2000",
     "Lu123.1"
   ),
   User4: createUser(
-    4,
+    
     "Victor",
     "Bertramvictor123@gmail.com",
     "22/08/2000",
