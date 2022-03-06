10630b40bb26de30dfe65270f68b484d498f8804
Bruno Pacheco <bruno.p94@gmail.com>
2022-03-06 09:36:07 -0300

    adicionada rotas do productSales

--- src/routes/v1/productsales.routes.js
+++ src/routes/v1/productsales.routes.js
@@ -2,10 +2,10 @@ const ProductSalesController = require("../../controllers/ProductSalesController
 const express = require("express");
 const { onlyCanAccessWith } = require("../../middlewares/auth");
 const { READ } = require("../../utils/constants/permissions");
-const product_salesRoutes = express.Router();
+const productsalesRoutes = express.Router();
 
-product_salesRoutes.post("/users", ProductSalesController.create);
-product_salesRoutes.patch("/sales/:sale_id/product/:product_id/amount/:amount", ProductSalesController.updateOne);
+productsalesRoutes.post("/users", ProductSalesController.create);
+productsalesRoutes.patch("/sales/:sale_id/product/:product_id/amount/:amount", ProductSalesController.updateOne);
 //salesRoutes.get("/users", onlyCanAccessWith([READ]), UserController.index);
 
-module.exports = product_salesRoutes;
+module.exports = productsalesRoutes;
