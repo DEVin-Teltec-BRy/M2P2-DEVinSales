dc67f52b81132b694dde0bdc3f9fb7413e6a5dcb
pedrohbsilva <phbs1235@gmail.com>
2022-03-06 17:17:58 -0300

    create sales migrate

--- src/database/migrations/20220302220504-create-sale.js
+++ src/database/migrations/20220302220504-create-sale.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Sales', {
+    await queryInterface.createTable('sales', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      seller_id: {
+      buyer_id: {
         type: Sequelize.INTEGER,
         references: {
           model: {
@@ -16,17 +16,17 @@ module.exports = {
           },
           key: 'id'
         },
+      
       },
-      buyer_id: {
+      seller_id: {
         type: Sequelize.INTEGER,
         allowNull:true,
         references: {
           model: {
-            tableName: 'users', 
+            tableName: 'users',
           },
           key: 'id'
         },
-        
       },
       dt_sale: {
         type: Sequelize.DATE
@@ -42,6 +42,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Sales');
+    await queryInterface.dropTable('sales');
   }
 };
