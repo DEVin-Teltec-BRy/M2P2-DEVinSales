95ddc79e826f56b3ea756f44785dd627a099c109
Bruno Pacheco <bruno.p94@gmail.com>
2022-03-06 18:20:04 -0300

    adição de regras no ProducSalesController para verificar se Product_id passado no params é o mesmo da Sale_id e amount>=0

--- src/controllers/ProductSalesController.js
+++ src/controllers/ProductSalesController.js
@@ -27,13 +27,25 @@ module.exports = {
           }
         }
       })
-      console.log(productSaleResult)
-        if(!saleResult || !productResult){
+        if(!saleResult || !productResult){ //confere se id de Venda e de Produto existem no banco
         return res.status(404).send({ message: "id de Produto ou de Venda não existem" });
 
       }else{
-        return res.status(201).send({ message: "Venda atualizada com sucesso." });
 
+        if(productSaleResult[0].dataValues.product_id!==Number(product_id)){ //confere se Produto repassado no Params é o mesmo cadastrado na Venda 
+          return res.status(400).send({message:"Id do produto enviado não é compatível ao cadastrado na venda."});
+        }else {
+          if(amount<=0||isNaN(amount)){ //confere se Produto repassado no Params é o mesmo cadastrado na Venda 
+            return res.status(400).send({message:"Quantidade deve ser um número superior à zero"});
+          }
+          //const id = Number(productSaleResult[0].dataValues.id)
+          //const saleInfo = await ProductsSales.findByPk(id)
+          console.log(productSaleResult[0].dataValues.amount)
+          productSaleResult[0].dataValues.amount = Number(amount)
+          console.log(productSaleResult[0].dataValues.amount)
+          const updatedAmount = await productSaleResult.save()
+        return res.status(201).send({ message: "Venda atualizada com sucesso." });
+        }
       }
       
 
