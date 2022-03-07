c5d76a9a97295478acbc4dedb3c807bade73a9ec
Guilherme Ferreira <guilherme.tecnologiaweb@gmail.com>
2022-03-07 17:19:22 -0300

    Buscando cidades por ID (params) ou NAME (query)

--- src/controllers/StateController.js
+++ src/controllers/StateController.js
@@ -1,10 +1,14 @@
 const State = require("../models/State");
+const City = require("./../models/City");
 const { validateErrors } = require("../utils/functions");
+const { Op } = require("sequelize");
 
 module.exports = {
   async getCitiesByStateID(req, res) {
     try {
       const { state_id } = req.params;
+      const { name } = req.query;
+
       const state = await State.findOne({
         where: {
           id: state_id,
@@ -15,7 +19,18 @@ module.exports = {
         return res.status(404).json({ message: "Estado não encontrado." });
       }
 
-      return res.status(200).json({ message: state });
+      const cities = await City.findAll({
+        where: {
+          state_id: {
+            [Op.eq]: state_id,
+          },
+          name: {
+            [Op.iLike]: `%${name}%`,
+          },
+        },
+      });
+
+      return res.status(200).json({ message: cities });
     } catch (error) {
       const message = validateErrors(error);
       return res.status(400).send(message);
