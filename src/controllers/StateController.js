9023d2b38ef6804527d75b9a0b4408b3e783ee1f
Guilherme Ferreira <guilherme.tecnologiaweb@gmail.com>
2022-03-07 16:59:21 -0300

    Verificando se existe o estado através do parâmetro state_id

--- src/controllers/StateController.js
+++ src/controllers/StateController.js
@@ -5,7 +5,17 @@ module.exports = {
   async getCitiesByStateID(req, res) {
     try {
       const { state_id } = req.params;
-      return res.status(200).json({ message: state_id });
+      const state = await State.findOne({
+        where: {
+          id: state_id,
+        },
+      });
+
+      if (!state) {
+        return res.status(404).json({ message: "Estado não encontrado." });
+      }
+
+      return res.status(200).json({ message: state });
     } catch (error) {
       const message = validateErrors(error);
       return res.status(400).send(message);
