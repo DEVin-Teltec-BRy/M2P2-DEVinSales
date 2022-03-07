4528de506e4da64c6cf455ab4e34e1a609dd3d78
Bruno Pacheco <bruno.p94@gmail.com>
2022-03-07 19:14:05 -0300

    adição do middleware de autenticação

--- src/routes/v1/productsales.routes.js
+++ src/routes/v1/productsales.routes.js
@@ -1,11 +1,11 @@
 const ProductSalesController = require("../../controllers/ProductSalesController");
 const express = require("express");
 const { onlyCanAccessWith } = require("../../middlewares/auth");
-const { READ } = require("../../utils/constants/permissions");
+const { UPDATE } = require("../../utils/constants/permissions");
 const productsalesRoutes = express.Router();
 
 productsalesRoutes.post("/users", ProductSalesController.create);
-productsalesRoutes.patch("/sales/:sale_id/product/:product_id/amount/:amount", ProductSalesController.updateOne);
+productsalesRoutes.patch("/sales/:sale_id/product/:product_id/amount/:amount", onlyCanAccessWith([UPDATE]),ProductSalesController.updateOne);
 //salesRoutes.get("/users", onlyCanAccessWith([READ]), UserController.index);
 
 module.exports = productsalesRoutes;
