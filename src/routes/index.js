e238e9b30feb54989ec75503cdcd213134f13656
priscillararimmy <priscillararimmy@hotmail.com>
2022-03-07 15:50:36 -0300

    Criação do AddressController e das rotas para o endereço

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const permissionRoutes = require('./v1/permissions.routes');
 const rolesRoutes = require('./v1/roles.routes');
 const usersRoutes = require('./v1/users.routes');
 const productsRoutes = require('./v1/products.routes');
-const usersRoutes = require('./v1/addresses.routes')
+const addressRoutes = require('./v1/addresses.routes')
 routes.get('/', (req, res) => {
     // #swagger.ignore = true
     res.redirect('/api/v1/docs')
