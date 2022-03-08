e87745f927c30f9cbec3c85f19b6615c94d176a7
Guilherme Ferreira <guilherme.tecnologiaweb@gmail.com>
2022-03-07 21:29:26 -0300

    Ajustando a busca por ID ou Name

--- src/controllers/StateController.js
+++ src/controllers/StateController.js
@@ -16,19 +16,34 @@ module.exports = {
       });
 
       if (!state) {
-        return res.status(404).json({ message: "Estado não encontrado." });
+        return res
+          .status(404)
+          .json({ message: "Nenhum resultado encontrado." });
+      }
+
+      const query = {
+        state_id,
+      };
+
+      if (name) {
+        query.name = {
+          [Op.iLike]: `%${name}%`,
+        };
       }
 
       const cities = await City.findAll({
-        where: {
-          state_id: {
-            [Op.eq]: state_id,
-          },
-          name: {
-            [Op.iLike]: `%${name}%`,
-          },
+        where: query,
+        include: {
+          association: { model: "states" },
+          attributes: ["name"],
         },
       });
+
+      if (cities.length < 1) {
+        return res
+          .status(404)
+          .json({ message: "Nenhum resultado encontrado." });
+      }
 
       return res.status(200).json({ message: cities });
     } catch (error) {
