97f03d7bf91bb0a0b7b0b820c801195e219e7694
Guilherme Ferreira <guilherme.tecnologiaweb@gmail.com>
2022-03-09 17:53:54 -0300

    Validando estado e cidade

--- src/controllers/StateController.js
+++ src/controllers/StateController.js
@@ -1,4 +1,5 @@
 const State = require("../models/State");
+const City = require("../models/City");
 const { validateErrors } = require("../utils/functions");
 
 module.exports = {
@@ -9,6 +10,28 @@ module.exports = {
     */
     try {
       const { state_id, city_id } = req.params;
+
+      const state = await State.findOne({
+        where: {
+          id: state_id,
+        },
+      });
+
+      if (!state) {
+        return res.status(404).json({ message: "Not Found." });
+      }
+
+      const city = await City.findOne({
+        where: {
+          id: city_id,
+        },
+      });
+
+      if (!city) {
+        return res.status(404).json({ message: "Not Found." });
+      }
+
+      return res.status(200).json({ state, city });
     } catch (error) {
       const message = validateErrors(error);
       return res.status(400).send(message);
