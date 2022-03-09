cfb5c4828600589caeda5806fc0a5629cb99e741
sabrinamaral <sabrinamaral@hotmail.com>
2022-03-08 22:15:13 -0300

    resolvendo conflito

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,10 +4,11 @@ const { Op } = require("sequelize");
 const { send } = require("express/lib/response");
 
 module.exports = {
-<<<<<<< HEAD
   async index(req, res) {
     /* #swagger.tags = ['Produto']
-    #swagger.description = 'Endpoint para buscar produtos conforme critério query params. Caso a busca seja feita sem os parâmetros, o endpoint irá retornar todos os produtos cadastrados.'
+    #swagger.description = 'Endpoint para buscar produtos conforme critério query params. 
+    Caso a busca seja feita sem os parâmetros, o endpoint irá retornar todos os produtos cadastrados. 
+    Nesse endpoint o usuário deve ter permissão READ.'
     #swagger.parameters['name'] = {
       in: 'query',
       description: 'query para buscar produtos pelo nome',
@@ -36,86 +37,86 @@ module.exports = {
       const priceMax = Number(price_max)
         ? Number(price_max)
         : Number.MAX_SAFE_INTEGER;
-=======
-    async index(req, res) {
-        // #swagger.tags = ['Produto']
-        // #swagger.description = 'Endpoint para buscar produtos conforme critério query params'
 
-        try {
-            const { name, price_min, price_max } = req.query
-            const query = {};
-            if (name) {
-                query.name = { [Op.iLike]: `%${name}%` };
-            }
-            const priceMin = Number(price_min) ? Number(price_min) : 0
-            const priceMax = Number(price_max) ? Number(price_max) : Number.MAX_SAFE_INTEGER
->>>>>>> master
+      if (priceMax <= priceMin) {
+        return res.status(400).json({
+          message: "Price max must be greater than price min",
+        });
+      }
+      query.suggested_price = {
+        [Op.between]: [priceMin, priceMax],
+      };
 
-            if (priceMax <= priceMin) {
-                return res.status(400).json({
-                    message: "Price max must be greater than price min"
-                })
-            }
-            query.suggested_price = {
-                [Op.between]: [priceMin, priceMax]
-            };
+      const products = await Product.findAll({
+        attributes: ["id", "name", "suggested_price"],
+        where: query,
+      });
 
-            const products = await Product.findAll({
-                attributes: ["id", "name", "suggested_price"],
-                where: query,
-            });
+      if (products.length === 0) return res.status(204).send();
 
-            if (products.length === 0) return res.status(204).send();
+      return res.status(200).send({ products });
+    } catch (error) {
+      const message = validateErrors(error);
+      return res.status(400).send(message);
+    }
+  },
 
-            return res.status(200).send({ products });
+  async putUpdate(req, res) {
+    /*#swagger.tags = ['Produto']
+    #swagger.description = 'Endpoint para atualizar um produt. Neste Endpoint o usuário logado deve ter permissão de UPDATE.'
+    #swagger.parameters['name'] = {
+      in: 'query',
+      description: 'query para buscar produtos pelo nome',
+      required: false,
+    }*/
 
-        } catch (error) {
-            const message = validateErrors(error);
-            return res.status(400).send(message)
-        }
-    },
+    try {
+      const { product_id } = req.params;
+      const { name, suggested_price } = req.body;
+      const nameData = name.trim();
 
-    async putUpdate(req, res){
-        // #swagger.tags = ['Produto']
-        // #swagger.description = 'Endpoint para atualizar um produto, neste Endpoint o usuário logado deve ter permissão de UPDATE.'
-        try {
-            const { product_id } = req.params
-            const {name, suggested_price} = req.body
-            const nameData = name.trim()
+      if (!nameData) {
+        return res
+          .status(400)
+          .send({ message: "O campo name, não foi enviado." });
+      }
+      if (!suggested_price) {
+        return res
+          .status(400)
+          .send({ message: "O campo suggested_price, não foi enviado." });
+      }
+      if (Number(suggested_price) <= 0) {
+        return res
+          .status(400)
+          .send({ message: "O campo suggested_price, deve ser maior que 0." });
+      }
+      const hasProductWithSameName = await Product.count({
+        where: {
+          name: nameData,
+          id: {
+            [Op.not]: product_id,
+          },
+        },
+      });
+      if (hasProductWithSameName) {
+        return res
+          .status(400)
+          .send({ message: "Já existe outro produto com o mesmo nome." });
+      }
+      const product = await Product.findByPk(product_id);
+      if (!product) {
+        return res.status(404).send({ message: "Product not found." });
+      }
+      product.name = nameData;
+      product.suggested_price = suggested_price;
 
-            if(!nameData) {
-                return res.status(400).send({message: 'O campo name, não foi enviado.'});
-            }
-            if(!suggested_price){
-                return res.status(400).send({message: 'O campo suggested_price, não foi enviado.'});
-            }
-            if(Number(suggested_price) <= 0) {
-                return res.status(400).send({message: 'O campo suggested_price, deve ser maior que 0.'});
-            }
-            const hasProductWithSameName = await Product.count({
-                where: {
-                    name : nameData,
-                    id: {
-                     [Op.not]: product_id
-                    }
-                }})
-            if(hasProductWithSameName){
-                return res.status(400).send({message: 'Já existe outro produto com o mesmo nome.'});
-            }
-            const product = await Product.findByPk(product_id)
-            if(!product) {
-                return res.status(404).send({message: 'Product not found.'});
-            }
-            product.name = nameData
-            product.suggested_price = suggested_price
+      await product.save();
 
-            await product.save()
-            
-            return res.status(204).send();
-        } catch (error) {
-            const message = validateErrors(error);
-            return res.status(400).send(message)
-        }
+      return res.status(204).send();
+    } catch (error) {
+      const message = validateErrors(error);
+      return res.status(400).send(message);
+    }
   },
   async delete(req, res) {
     /* #swagger.tags = ['Produto'],
