39b6d2d625be0a86fa4a9bb0f5e9de6a2e70f4dc
claudiorhenns <69265180+claudiorhenns@users.noreply.github.com>
2022-03-10 10:40:58 -0300

    feito o merge na develpment

--- src/routes/v1/products.routes.js
+++ src/routes/v1/products.routes.js
@@ -5,6 +5,6 @@ const { onlyCanAccessWith } = require("../../middlewares/auth");
 const { READ, DELETE, UPDATE } = require("../../utils/constants/permissions");
 
 productsRoutes.delete("/products/:id", onlyCanAccessWith([DELETE]), ProductController.delete);
-productsRoutes.put("/product/:product_id", onlyCanAccessWith([UPDATE]), ProductController.update);
+//productsRoutes.put("/product/:product_id", onlyCanAccessWith([UPDATE]), ProductController.update);
 
 module.exports = productsRoutes;
