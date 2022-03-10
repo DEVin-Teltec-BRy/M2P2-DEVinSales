64b4b5838ce1110f8331afa30a70d9520c26e0aa
raulvictorvieira <raulvictorsv@gmail.com>
2022-03-09 21:24:11 -0300

    documentação do endpoint

--- src/controllers/AddressController.js
+++ src/controllers/AddressController.js
@@ -5,6 +5,28 @@ const { Op } = require("sequelize");
 module.exports={
 
     async index(req,res){
+        
+        // #swagger.tags = ['Endereços']
+        // #swagger.description = 'Endpoint que retorna os endereços com base nos dados fornecidos via query, ou então todos os endereços caso nenhuma query seja passada'
+
+        // #swagger.parameters['city_id'] = {
+        //   in: 'query',
+        //   description: 'Filtro que identifica o id da cidade desejada',
+        //   type: 'number',
+        //   collectionFormat: 'multi',
+        // }
+        // #swagger.parameters['street'] = {
+        //   in: 'query',
+        //   description: 'Filtro que identifica o nome da rua que será retornada',
+        //   type: 'string',
+        //   collectionFormat: 'multi',
+        // }
+        // #swagger.parameters['cep'] = {
+        //   in: 'query',
+        //   description: 'Filtro que identifica o cep que será retornada',
+        //   type: 'string',
+        //   collectionFormat: 'multi',
+        // }
 
         try {
             const {
@@ -37,14 +59,17 @@ module.exports={
             });
       
             if (addresses.length === 0) {
+                // #swagger.responses[204] = { description: 'No Content' }
                 return res.status(204).json({ message: "No Content" });
             } else {
+                // #swagger.responses[200] = { description: 'Success!' }
                 return res.status(200).json({ addresses });
             }
 
           } catch (error) {
               const message = validateErrors(error);
-              return res.status(400).send(message);
+              // #swagger.responses[403] = { description: 'Forbidden' }
+              return res.status(403).send(message);
           }
 
 }
