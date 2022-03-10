0316054d1428ac37042ea2a78b610f674a991e3b
Guilherme Ferreira <guilherme.tecnologiaweb@gmail.com>
2022-03-10 19:55:18 -0300

    Finalizando as funções para o tratamento da query

--- src/controllers/StateController.js
+++ src/controllers/StateController.js
@@ -1,22 +1,7 @@
 const State = require("../models/State");
 const City = require("../models/City");
 const { validateErrors } = require("../utils/functions");
-const { Op, where, fn, col, literal } = require("sequelize");
-//const { Sequelize } = require("sequelize");
-
-/*
-const db = require("./../database");
-
-const sequelize = new Sequelize(
-  db.config.database,
-  db.config.username,
-  db.config.password,
-  {
-    host: db.config.host,
-    dialect: db.options.dialect,
-  }
-);
-*/
+const { Op, where, fn, col } = require("sequelize");
 
 module.exports = {
   async getCitiesByStateID(req, res) {
@@ -28,7 +13,6 @@ module.exports = {
       const { state_id } = req.params;
       const { name } = req.query;
 
-      let result = "";
       const accent =
         "Á,À,Ã,Â,Ä,á,à,ã,â,ä,É,È,Ê,Ë,é,è,ê,ë,Í,Ì,Î,Ï,í,ì,î,ï,Ó,Ò,Ô,Õ,Ö,ó,ò,ô,õ,ö,Ú,Ù,Û,Ü,ú,ù,û,ü,Ç,ç,Ñ,ñ";
       const unaccent =
@@ -49,23 +33,15 @@ module.exports = {
       };
 
       if (name) {
-        query.name = {
-          [Op.iLike]: `%${name}%`,
-          // where(fn("translate", "name", accent, unaccent)),
-        };
+        query.name = where(
+          fn("translate", fn("lower", col("City.name")), accent, unaccent),
+          {
+            [Op.iLike]: `%${name
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")}%`,
+          }
+        );
       }
-
-      // const lowerCase = fn("lower", name);
-      // const replacedSpaces = fn("replace", name, "a", "b");
-      // fn("lower", col("name"))
-      // $and: where(),
-
-      // fn('regexp_replace', accent, "E'[^\\w -]'", '', 'g')
-      // fn("lower", col("name")),
-      // fn("replace", lowerCase, accent, unaccent)
-      // fn("translate", name, accent, unaccent)
-
-      //fn("translate", name , accent, unaccent)),
 
       const cities = await City.findAll({
         where: query,
@@ -79,27 +55,9 @@ module.exports = {
         ],
       });
 
-      if (cities.length < 1) {
-        return res.status(404).json({ message: "Not Found." });
+      if (!cities.length) {
+        return res.status(204).json({ message: "Not Content." });
       }
-
-      /*
-      if (name) {
-        result = await sequelize.query(
-          `SELECT c.id, c.name as city, c.state_id, s.name as state, s.initials FROM cities c INNER JOIN states s ON c.state_id = s.id WHERE c.state_id = ${state_id} and LOWER(TRANSLATE(c.name, '${accent}', '${unaccent}')) = LOWER(TRANSLATE('${name}', '${accent}', '${unaccent}'))`
-        );
-      } else {
-        result = await sequelize.query(
-          `SELECT c.id, c.name as city, c.state_id, s.name as state, s.initials FROM cities c INNER JOIN states s ON c.state_id = s.id WHERE c.state_id = ${state_id}`
-        );
-      }
-
-      if (!result[0].length) {
-        return res.status(204).json({ message: "No Content." });
-      }
-
-      return res.status(200).json({ result: result[0] });
-      */
 
       return res.status(200).json({ cities });
     } catch (error) {
