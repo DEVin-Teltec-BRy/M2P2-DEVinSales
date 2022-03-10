3412e04b57e2c3f84e706c3d8fb5f6b52f7780e0
matheusgzamboni <matheusgzamboni@gmail.com>
2022-03-10 00:04:32 -0300

    Adiciona rota para endpoint POST insertNewAddress. As rotas com autenticação estão comentadas

--- src/routes/v1/states.routes.js
+++ src/routes/v1/states.routes.js
@@ -1,10 +1,12 @@
 const StateController = require("../../controllers/StateController");
 const express = require("express");
 const { onlyCanAccessWith } = require("../../middlewares/auth");
-const { READ } = require("../../utils/constants/permissions");
+const { READ, WRITE } = require("../../utils/constants/permissions");
 const statesRoutes = express.Router();
 
-statesRoutes.get("/state", onlyCanAccessWith([READ]), StateController.index);
-
+// statesRoutes.get("/state", onlyCanAccessWith([READ]), StateController.index);
+statesRoutes.get("/state", StateController.index);
+// statesRoutes.post("/state/:state_id/city/:city_id/address", onlyCanAccessWith([WRITE]), StateController.insertNewAddress);
+statesRoutes.post("/state/:state_id/city/:city_id/address", StateController.insertNewAddress);
 
 module.exports = statesRoutes;
