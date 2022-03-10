63d864d89447510177716e4bfde780d63a0ab0c7
chicodevbr <chicodevbr@gmail.com>
2022-03-10 10:51:55 -0300

    retirando as permissions para testar o endpoint

--- src/routes/v1/addresses.routes.js
+++ src/routes/v1/addresses.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const AddressController = require('../../controllers/AddressController');
-const { onlyCanAccessWith } = require('../../middlewares/auth')
+const { onlyCanAccessWith } = require('../../middlewares/auth');
 const permission = require('../../utils/constants/permissions');
 
 const addressesRoutes = express.Router();
 
-addressesRoutes.get('/address', onlyCanAccessWith([permission.READ]), AddressController.index);
+addressesRoutes.get('/address', AddressController.index);
 
 module.exports = addressesRoutes;
