fc636cdc6f5f0c82db0917e62a9495b76274869f
Daniel Meireles <daniel@inovadora.com.br>
2022-03-11 16:23:48 -0300

    Adicionando validação de acesso as novas rotas.

--- src/routes/v1/roles.routes.js
+++ src/routes/v1/roles.routes.js
@@ -1,9 +1,13 @@
 const RoleController = require('../../controllers/RoleController');
-const express = require('express')
+const express = require('express');
+const { isOwner, onlyCanAccessWith } = require('../../middlewares/auth');
+const { WRITE } = require('../../utils/constants/permissions');
 const rolesRoutes = express.Router();
 
-rolesRoutes.post('/roles', RoleController.create);
-rolesRoutes.get('/roles', RoleController.index);
-rolesRoutes.post('/roles/:role_id', RoleController.addPermissions);
+rolesRoutes.post('/roles', isOwner, RoleController.create);
+
+rolesRoutes.get('/roles', onlyCanAccessWith([WRITE]), RoleController.index);
+
+rolesRoutes.post('/roles/:role_id', isOwner, RoleController.addPermissions);
 
 module.exports = rolesRoutes;
