f4d26465c2e2bbb936cf71d3be2844ed1b89a770
amandaamabili <amanda_amabilly.b@hotmail.com>
2022-03-10 21:13:51 -0300

    copy method saleMade

--- src/controllers/SaleController.js
+++ src/controllers/SaleController.js
@@ -58,24 +58,79 @@ module.exports = {
     },
 
     async saleMade(req, res) {
-        const { seller_id } = req.params;
-           // const { authorization } = req.headers;
-            const  {sales_id, product_id, unit_price, amount } = req.body;
         try {
-            console.log('slslsl')
+            // #swagger.tags = ['Sales']
+            // #swagger.description = 'nao sei o que ele faz.'
+            const { seller_id } = req.params;
+            const { product_id } = req.body;
+            const { authorization } = req.headers;
+            let { unit_price, amount } = req.body;
+            const dt_sale = new Date();
+            const buyer = await verify(authorization, process.env.SECRET);
+            const buyer_id = buyer.userId;
+            // se o amount vir vazio bota o valor de 1
+            if (!amount || amount.replace(/\s/g, "") == "") {
+                amount = 1;
+            }
+
+            //verificando se o product_id foi enviado
+            if (
+                !product_id ||
+                product_id.replace(/\s/g, "") == "" ||
+                product_id === "any"
+            ) {
+                return res.status(400).send({ message: "Product_id invalido" });
+            }
+
+            //verificando se o amount ou o unit price estao com valores menores que 0
+            if (unit_price <= 0 || amount <= 0) {
+                return res
+                    .status(400)
+                    .send({ message: "unit_price ou amount com valores invalidos" });
+            }
+
+            //verificando se o product id existe para
+            const validProductId = await Product.findByPk(product_id);
+            if (!validProductId) {
+                return res.status(404).send({ message: "product_id inexistente" });
+            }
+
+            //verificando se o seller_id existe na tabela user_id
+            const validSellerId = await User.findByPk(seller_id);
+            if (!validSellerId) {
+                return res.status(404).send({ message: "seller_id inexistente" });
+            }
+
+            //verificando se o unit_price foi enviado
+            if (
+                !unit_price ||
+                unit_price.replace(/\s/g, "") == "" ||
+                unit_price === "any"
+            ) {
+                unit_price = validProductId.suggested_price;
+            }
+            //Criando o registro sale
+            const sale = await Sale.create({
+                seller_id,
+                buyer_id,
+                dt_sale,
+            });
+            const sales_id = sale.dataValues.id;
+
             //criando o registro product_sale
             const product_sale = await ProductsSales.create({
-                sales_id :sales_id,
-                product_id :product_id,
-                unit_price :unit_price,
-                amount :amount,
+                sales_id,
+                product_id,
+                unit_price,
+                amount,
             });
-          console.log(product_sale)
-            res.status(201).send({ 'created': "id-" + product_sale.id });
+            const product_sale_id = product_sale.dataValues.id;
+
+            return res.status(201).send(`product_sale id: ${product_sale_id}`);
         } catch (error) {
             return res.status(400).send(error.message);
         }
-    }
+    },
 }
 
 
