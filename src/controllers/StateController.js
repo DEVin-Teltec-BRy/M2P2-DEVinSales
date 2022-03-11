6564de7b1774b415cce72e5e9978e29627529452
priscillararimmy <priscillararimmy@hotmail.com>
2022-03-11 13:36:54 -0300

    "Alterações no endpoint state"

--- src/controllers/StateController.js
+++ src/controllers/StateController.js
@@ -14,15 +14,12 @@ module.exports = {
             return res.status(404).send({message: "Nenhum Estado foi encontrado"})
         }
 
-
         const { name } = req.body
         const city = await City.findOne({
             where: {
                 name
             }
         })
-
-        // console.log(city.name)
 
         if(city.name === name ){
             return res.status(400).send({message: "Esta cidade já existe."})
@@ -47,21 +44,3 @@ module.exports = {
     }
 
 }
-// 6 - Endpoint: POST /state/{state_id}/city
-// Entrada:
-// ● state_id: integer (Path parameter)
-// ● Body:
-// {
-// name: string
-// }
-// Regras de negócio:
-// ● O usuário logado deve possuir a autorização de WRITE para o endpoint
-// de Cidade. Caso não possua, deve-se retornar o Status de Erro 403
-// (Forbidden) okay
-// ● Caso não exista nenhum registro de Estado com o state_id enviado,
-// deve ser retornado o Status de Erro 404 (Not Found) okay
-// ● Caso exista alguma outra cidade criada nesse estado com o mesmo
-// nome enviado, deve ser retornado o Status de Erro 400 (Bad Request)
-// ● Caso todas as validações passem, deve ser criada uma cidade com
-// nome e estado especificados, retornando o Id da cidade criada, com o
-// Status 201 (Created)
