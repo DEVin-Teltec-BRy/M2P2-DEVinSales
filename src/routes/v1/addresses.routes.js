d2be349b695af222c55b481ac257f23b45951164
raulvictorvieira <raulvictorsv@gmail.com>
2022-03-11 21:56:25 -0300

    inclusão da autorização de update na routes

--- src/routes/v1/addresses.routes.js
+++ src/routes/v1/addresses.routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const AddressController = require('../../controllers/AddressController');
 const { onlyCanAccessWith } = require('../../middlewares/auth')
-const { READ, WRITE } = require('../../utils/constants/permissions');
+const { READ, UPDATE } = require('../../utils/constants/permissions');
 
 const addressesRoutes = express.Router();
 
 addressesRoutes.get('/address', onlyCanAccessWith([READ]), AddressController.index);
-addressesRoutes.patch('/address/:address_id', AddressController.update);
+addressesRoutes.patch('/address/:address_id', onlyCanAccessWith([UPDATE]), AddressController.update);
 
 module.exports = addressesRoutes;
