d5e42cb628510319b4bf2a168242e0d3329beb9e
matheusgzamboni <matheusgzamboni@gmail.com>
2022-03-13 14:56:02 -0300

    Cria endpoint para inserção de novo endereço, com documentação swagger

--- src/controllers/AddressController.js
+++ src/controllers/AddressController.js
@@ -7,29 +7,29 @@ const { Op } = require("sequelize");
 module.exports={
 
     async index(req,res){
-        
-        // #swagger.tags = ['Endereços']
-        // #swagger.description = 'Endpoint que retorna os endereços com base nos dados fornecidos via query, ou então todos os endereços caso nenhuma query seja passada'
+        /*
+          #swagger.tags = ['Endereços']
+          #swagger.description = 'Endpoint que retorna os endereços com base nos dados fornecidos via query, ou então todos os endereços caso nenhuma query seja passada'
 
-        // #swagger.parameters['city_id'] = {
-        //   in: 'query',
-        //   description: 'Filtro que identifica o id da cidade desejada',
-        //   type: 'number',
-        //   collectionFormat: 'multi',
-        // }
-        // #swagger.parameters['street'] = {
-        //   in: 'query',
-        //   description: 'Filtro que identifica o nome da rua que será retornada',
-        //   type: 'string',
-        //   collectionFormat: 'multi',
-        // }
-        // #swagger.parameters['cep'] = {
-        //   in: 'query',
-        //   description: 'Filtro que identifica o cep que será retornada',
-        //   type: 'string',
-        //   collectionFormat: 'multi',
-        // }
-
+          #swagger.parameters['city_id'] = {
+            in: 'query',
+            description: 'Filtro que identifica o id da cidade desejada',
+            type: 'number',
+            collectionFormat: 'multi',
+          }
+          #swagger.parameters['street'] = {
+            in: 'query',
+            description: 'Filtro que identifica o nome da rua que será retornada',
+            type: 'string',
+            collectionFormat: 'multi',
+          }
+          #swagger.parameters['cep'] = {
+            in: 'query',
+            description: 'Filtro que identifica o cep que será retornada',
+            type: 'string',
+            collectionFormat: 'multi',
+          }
+        */
         try {
             const {
                 city_id,
@@ -74,8 +74,52 @@ module.exports={
               return res.status(403).send(message);
         }
     },
-    
+
     async insertNewAddress(req, res) {
+          /*
+            #swagger.tags = ['Endereços']
+            #swagger.description = 'Endpoint para adicionar um novo endereço ao banco de dados'
+            #swagger.consumes = ['application/json']
+            #swagger.parameters['state_id'] = {
+              in: 'path',
+              description: 'Filtro que identifica o id do estado no qual o endereço está localizado',
+              type: 'integer',
+            }
+            #swagger.parameters['city_id'] = {
+              in: 'path',
+              description: 'Filtro que identifica o id da cidade na qual o endereço está localizado',
+              type: 'integer',
+            }
+            #swagger.parameters['obj'] = { 
+                in: 'body', 
+                required: 'true',
+                '@schema': { 
+                    "required": ["street", "number", "cep"], 
+                    "properties": { 
+                        "street": { 
+                            "required": true,
+                            "type": "string",
+                            "example": "Rua Florianopolis", 
+                        },
+                        "number": {
+                            "required": true,
+                            "type": "number",
+                            "example": 123,
+                        },
+                        "cep": {
+                            "required": true,
+                            "type": "string",
+                            "example": "89229780", 
+                        },
+                        "complement": {
+                            "required": false,
+                            "type": "string",
+                            "example": "Apto. 302", 
+                        },
+                    } 
+                } 
+            } 
+        */
         try {
           const {state_id, city_id} = req.params;
           const addressData = req.body;
@@ -114,17 +158,83 @@ module.exports={
           if(city[0].state_id !== state[0].id) {
             return res.status(400).send({message: "The 'city_id' returned a city that doesn't match with the given 'state_id'"})
           }
-    
-          // *************** TESTE ***************
-          
+              
           //Valida as chaves de endereço
-          const validKeys = ['street', 'number', 'complement', 'cep']
-          const addressObjKeys = Object.keys(addressData);
+          const addressObjKeys = ['street', 'number', 'cep']
+          if(addressObjKeys.every(key => key in addressData)) {
+            // Valida typeof street
+            if(typeof addressData.street !== 'string') {
+              return res.status(400).send({message: "The 'street' param must be a string"})
+            } else if (addressData.street.length === 0) {
+              return res.status(400).send({message: "The 'street' param cannot be empty"})
+            }
+
+            // Valida typeof number
+            if(isNaN(addressData.number)) {
+              return res.status(400).send({message: "The 'number' param must be a number"})
+            }
+
+            //Valida cep
+            if(typeof addressData.cep !== 'string') {
+              return res.status(400).send({message: "The 'street' param must be a string"})
+            } 
+            else if (addressData.cep.length < 8 || addressData.cep.length > 9) {
+              return res.status(400).send({message: "The 'cep' param is invalid"})
+            }
+            else if (addressData.cep.length === 9) {
+              if(addressData.cep[5] !== '-') {
+                return res.status(400).send({message: "The 'cep' param format is invalid"})
+              }
+            }
+          } 
+          else {
+            return res.status(400).send({message: "The 'street', 'number' and 'cep' params are required in the req body"})
+          }
+
+          // Verifica se o endereço já existe no DB
+          const checkDuplicate = await Address.findAll({
+            where: { 
+              [Op.and] : [{
+                street: {
+                  [Op.iLike]: `${addressData.street}`
+                },
+                number: {
+                  [Op.eq]: addressData.number
+                },
+                cep: {
+                  [Op.iLike]: `${addressData.cep}`
+                },
+                city_id: {
+                  [Op.eq]: `${city[0].id}`
+                },
+              }]
+            }
+          });
           
+          if(checkDuplicate.length) {
+            return res.status(200).send({ message: "Endereço já existente! Não foi possível adicionar o endereço.", address_id: checkDuplicate[0].id});
+          }
 
+          // Cria objeto que será criado no DB
+          const newAddress =  addressData.hasOwnProperty('complement') ? 
+          {
+            city_id: city[0].id, 
+            street: addressData.street, 
+            number: addressData.number, 
+            complement: addressData.complement,
+            cep: addressData.cep
+          } : {
+            city_id: city[0].id, 
+            street: addressData.street, 
+            number: addressData.number, 
+            complement: "",
+            cep: addressData.cep
 
-          return res.status(200).send({ state, city, addressData, addressDataKeys });
-    
+          };
+
+          const address = await Address.create(newAddress)
+          return res.status(201).send({address_id: address.id});
+
         } catch (error) {
           const message = validateErrors(error);
           return res.status(400).send(message);
