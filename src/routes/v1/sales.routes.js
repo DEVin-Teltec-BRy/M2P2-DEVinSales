7ab12704ba6e61e010d99e8f9cbf218dd2584c8a
Ricardo Augusto Lima <rickgusto1986@gmail.com>
2022-03-14 18:58:34 -0300

    ajustes de conflitos

--- src/routes/v1/sales.routes.js
+++ src/routes/v1/sales.routes.js
@@ -6,7 +6,8 @@ const { READ, WRITE, UPDATE, DELETE } = require("../../utils/constants/permissio
 
 
 
-salesRoutes.get('/user/:id/sales',onlyCanAccessWith([READ]) ,SaleController.showSaler);
+salesRoutes.get('/user/:id/sales', onlyCanAccessWith([READ]), SaleController.showSaler);
+
 salesRoutes.get('/user/:user_id/buy',onlyCanAccessWith([READ]), SaleController.showSalesByBuyer);
 salesRoutes.post('/sales/:seller_id/saleMade',onlyCanAccessWith([WRITE]),SaleController.saleMade);
 salesRoutes.post('/user/:user_id/sales', SaleController.create);
