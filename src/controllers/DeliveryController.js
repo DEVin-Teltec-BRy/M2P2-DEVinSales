ad8d53b1cabbbb53a884e5296a77bc88868e2904
Guilherme Gomes <90911760+Guigomes2@users.noreply.github.com>
2022-03-17 10:06:10 -0300

    Alteraçoes Endpoint Deliveries

--- src/controllers/DeliveryController.js
+++ src/controllers/DeliveryController.js
@@ -1,3 +1,7 @@
+const Permission = require("../models/Permission");
+const { validateErrors } = require("../utils/functions");
+
+
 
 
 module.exports = {
@@ -6,8 +10,9 @@ module.exports = {
         // #swagger.description = 'Endpoint para buscar deliveries conforme critério query params'
         try {
             const { address_id, sale_id } = req.query
+            await Permission.create({description})
            
-            return res.status(200).send({message: 'Ola!'})
+            return res.status(200).send({message: 'Ok'})
         } catch (error) {
             const message = validateErrors(error)
             return res.status(400).send(message)
